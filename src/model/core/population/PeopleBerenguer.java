be2cc56f893380a8a218e790b23be44a7e8f7599
berenguer <berenguer@users.noreply.github.com>
2015-01-17 15:58:15 +0100

    calcul satisfaction Berenguer

--- src/model/core/population/PeopleBerenguer.java
+++ src/model/core/population/PeopleBerenguer.java
@@ -1,23 +1,51 @@
 package model.core.population;
 
+import java.util.ArrayList;
+
 import model.core.Agent;
 import model.core.Environment;
 
 public class PeopleBerenguer extends Agent {
     
-    public int neighbors;
-    
-    public int type;
+    /**
+     * How many neighbors share the same type around me.
+     */
+    public int satisfaction;
 
-    public PeopleBerenguer(int posX, int posY, Environment environment, int neighbors, int type) {
+    public PeopleBerenguer(int posX, int posY, Environment environment, int satisfaction) {
         super(posX, posY, environment);
-        this.neighbors = neighbors;
-        this.type = type;
+        this.satisfaction = satisfaction;
     }
 
     @Override
     public void action() {
-        this.neighbors = this.environment.search(this.posX, this.posY, this.getClass()).size();
+        ArrayList<int[]> neighborsPositions = this.environment.search(this.posX, this.posY, this.getClass());
+        // count agents with the same type around my position
+        int sameTypeCounter = 0;
+        for (int[] n : neighborsPositions) {
+            if (this.environment.grid[n[0]][n[1]] != null) {
+                if (this.environment.grid[n[0]][n[1]].getClass() == this.getClass()) {
+                    sameTypeCounter++;
+                }
+            }
+        }
+        // change position if minimal satisfactio is not reached
+        if (sameTypeCounter < this.satisfaction) {
+            move();
+        }
+        
+    }
+    
+    public void move() {
+        // to do
+    }
+
+    public int getSatisfaction() {
+        return satisfaction;
+    }
+
+    public void setSatisfaction(int neighbors) {
+        this.satisfaction = neighbors;
     }
 
 }
